7e2531d1a6d96f916c72bf358e5221fbe3237639
Zahra Akhlaghi <z.akhlaghi@jdro.ir>
2022-05-18 23:33:24 +0430

    create producer callback

---
+++ src/main/java/com/simplesteph/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,50 @@
+package com.simplesteph.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        String bootstrapServer = "127.0.0.1:9092";
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            //create producer record
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello " + String.valueOf(i));
+
+            //send data
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received new metadata.\n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else
+                        logger.error("Error while producing.\n" + e);
+                }
+            });
+        }
+        //flush data
+        producer.flush();
+
+        //flush and close
+        producer.close();
+    }
+}
